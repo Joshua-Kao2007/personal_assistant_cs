e891bddf604f627d2c6c58e92b908aec4076f6f6
Joshua Kao <joshuakao@Joshuas-MacBook-Pro-2.local>
2025-05-23 19:40:05 -0700

    Initial commit of PROJECTQ# Flask app

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, request, jsonify, render_template, Response
+from flask import Flask, request, jsonify, render_template, Response # type: ignore
 from openai import OpenAI
 import os
-import cv2
+import cv2 # type: ignore
 import base64
 from pymongo import MongoClient
 from datetime import datetime
@@ -223,4 +223,5 @@ def get_emotion_entries():
 # --------------------------
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
+
